7bd66c7881c41bd142e2bb9810f6638063a76446
allison <allison.durham@gmail.com>
2021-07-15 01:51:04 -0400

    should have been making more commits

---
+++ tutorial/src/game.js
@@ -0,0 +1,33 @@
+import Paddle from "./paddle.js";
+import InputHandler from "./input.js";
+import Ball from "./ball.js";
+import Brick from "./brick.js"
+import { buildLevel, level1 } from "./levels.js";
+
+export default class Game {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+    }
+    
+    start() {
+        this.ball = new Ball(this);
+        this.paddle = new Paddle(this);
+
+        let bricks = buildLevel(this, level1);
+        
+        this.gameObjects = [this.ball, this.paddle, ...bricks];
+
+        new InputHandler(this.paddle);
+    }
+
+    update(deltaTime) {
+        this.gameObjects.forEach((object) => object.update(deltaTime));
+
+        this.gameObjects = this.gameObjects.filter(object => !object.markedForDeletion);
+    }
+    
+    draw(ctx) {
+        this.gameObjects.forEach((object) => object.draw(ctx));
+    }
+}
--- tutorial/src/paddle.js
+++ tutorial/src/paddle.js
@@ -2,8 +2,8 @@
 
 
 export default class Paddle {
-    constructor(gameWidth, gameHeight) {
-        this.gameWidth = gameWidth;
+    constructor(game) {
+        this.gameWidth = game.gameWidth;
         this.width = 150;
         this.height = 20;
         
@@ -11,8 +11,8 @@ this.maxSpeed = 7;
 this.speed = 0;
 
         this.position = {
-            x: gameWidth / 2 - this.width / 2,
-            y: gameHeight - this.height -10,
+            x: game.gameWidth / 2 - this.width / 2,
+            y: game.gameHeight - this.height -10,
         }
     }
 
@@ -31,7 +31,6 @@ this.speed = 0;
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
     update(deltaTime) {
-        if (!deltaTime) return;
 
         this.position.x += this.speed;
 
